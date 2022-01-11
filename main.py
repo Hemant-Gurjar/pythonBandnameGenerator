42c850ddf95b82184ec55cd36a927caa915f1504
Hemant <73576412+Hemant-Gurjar@users.noreply.github.com>
2022-01-11 04:31:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+city = input("What's name of the city you grew up in?\n")
+
+pet = input("What's the name of your pet?\n")
+
+print("The name of the Band can be " + city + " " + pet)
